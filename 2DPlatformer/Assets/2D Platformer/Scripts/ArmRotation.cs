0911caa479db2b7aa54959e460017c664867652b
Mellonkhv <mellonkhv@gmail.com>
2015-03-24 08:52:48 +1000

    123

---
+++ 2DPlatformer/Assets/2D Platformer/Scripts/ArmRotation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArmRotation : MonoBehaviour {
+	
+    public int rotationOffcet = 90;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		// Вычитание позиции игрока с позиции мыши
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();     // Нормализция вектора. Это означает, что все сумма вектора будет равна 1
+
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotationOffcet);
+	}
+}
