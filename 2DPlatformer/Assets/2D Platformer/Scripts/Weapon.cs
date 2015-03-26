56e7166342aa669f7bc2b863baaa7ff059afd5cb
Mellonkhv <mellonkhv@gmail.com>
2015-03-26 14:55:46 +1000

    2D Platformer - Полёт пули

--- 2DPlatformer/Assets/2D Platformer/Scripts/Weapon.cs
+++ 2DPlatformer/Assets/2D Platformer/Scripts/Weapon.cs
@@ -7,6 +7,10 @@ public class Weapon : MonoBehaviour {
     public float Damage = 10;
     public LayerMask whatToHit;
 
+    public Transform BulletTrailPrefab;
+    float timeToSpawnEffect = 0;
+    public float effectSpawnRate = 10;
+    
     float timeToFire = 0;
     Transform firePoint;
 
@@ -47,11 +51,21 @@ public class Weapon : MonoBehaviour {
         Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
         Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
         RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
-        Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100, Color.cyan);
+        if (Time.time >= timeToSpawnEffect)
+        {
+            Effect();
+            timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+        }
+       // Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100, Color.cyan);
         if (hit.collider != null)
         {
             Debug.DrawLine(firePointPosition, hit.point, Color.red);
             Debug.Log("Мы попали " + hit.collider.name + " и нанесли " + Damage + " урона.");
         }
+    }
+
+    void Effect()
+    {
+        Instantiate(BulletTrailPrefab, firePoint.position, firePoint.rotation);
     }
 }
