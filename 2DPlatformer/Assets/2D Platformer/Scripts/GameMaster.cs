3f02c5ec6ad11ce82a0b14077803d88ac2c0cab5
Mellonkhv <mellonkhv@gmail.com>
2015-03-27 15:05:36 +1000

    2D Platformer - Перерождение игрока

--- 2DPlatformer/Assets/2D Platformer/Scripts/GameMaster.cs
+++ 2DPlatformer/Assets/2D Platformer/Scripts/GameMaster.cs
@@ -3,8 +3,31 @@ using System.Collections;
 
 public class GameMaster : MonoBehaviour {
 
+    public static GameMaster gm;
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public int spawnDelay = 2;
+
+    void Start()
+    {
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    public IEnumerator RespawnPlayer ()
+    {
+        Debug.Log("TODO: Add waiting for spawn sound");
+        yield return new WaitForSeconds(spawnDelay);
+        
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        Debug.Log("TODO: Add Spawn Particles");
+    }
+
 	public static void KillPlayer (Player player)
     {
         Destroy(player.gameObject);
+        gm.StartCoroutine(gm.RespawnPlayer());
     }
 }
