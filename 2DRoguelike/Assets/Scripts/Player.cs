84b2d041a510fd45144f47a21a799e9605714b29
Mellonkhv <mellonkhv@gmail.com>
2015-04-08 16:50:28 +1000

    2D Roguelike - Комментарии к классу Player

--- 2DRoguelike/Assets/Scripts/Player.cs
+++ 2DRoguelike/Assets/Scripts/Player.cs
@@ -12,92 +12,127 @@ public class Player : MovingObject {
     private int food;                       // Количество 
 
 
-	// Use this for initialization
+    // Start перекрывает метод базового класса
 	protected override void Start () 
     {
         animator = GetComponent<Animator>();
 
+        // Получить текущую количество очков пища хранится в GameManager.instance между уровнями.
         food = GameManager.instance.playerFoodPoins;
-
+        // Вызвать метод Start из базового класса
         base.Start();
 	}
 
+    // Эта функция вызывается, когда игрок станет заблокированным или неактивным.
     private void OnDisable()
     {
+        // Сохраняем информацию о количестве еды
         GameManager.instance.playerFoodPoins = food;
     }
 	
 	// Update is called once per frame
 	void Update () 
     {
+        //  Если это не ход игрока, выйти из функции.
         if (!GameManager.instance.playerTurn) return;
         
-        int horizontal = 0;
-        int vertical = 0;
+        int horizontal = 0; // Использовать для горизонтального перемещения
+        int vertical = 0;   // Использовать для вертикального перемещения
 
+        // Получить информацию от менеджера ввода, вокруг него на целое и в магазине в горизонтальной установить по оси Х направлению движения
         horizontal = (int)Input.GetAxisRaw("Horizontal");
+        // Получить информацию от менеджера ввода, вокруг него на целое и в магазине в вертикальной установить по оси Y направлению движения
         vertical = (int)Input.GetAxisRaw("Vertical");
 
+        // Если двигаемся горизонтально
         if (horizontal != 0)
-            vertical = 0;
+            vertical = 0; // по вертикали запрещаем движение
+        // Если Игрок движется
         if (horizontal != 0 || vertical != 0)
+            // Вызов AttemptMove с общм параметром Wall, так это то с чем игрок может взаимодействовать, если они сталкиваются друг с другом (атакуя его).
             AttemptMove<Wall>(horizontal, vertical);
 	}
-
+    // Заменяем базовый класс AttemptMove
     protected override void AttemptMove <T> (int xDir, int yDir)
     {
+        // Каждый ход отимаем еду
         food--;
-
+        // Вызываем AttemptMove базового класса
         base.AttemptMove<T>(xDir, yDir);
-
+        // Hit позволяет ссылаться на результат Linecast сделано в движении.
         RaycastHit2D hit;
 
+        // Поскольку игрок перемещается и теряет очки еды, проверьте, жив ли он.
         CheckIfGameOver();
 
+        // Ход игрока закончился
         GameManager.instance.playerTurn = false;
     }
 
+    // OnTriggerEnter2D посылается, когда другой объект входит в пусковой коллайдер присоединены к этому объекту (только 2D физики).
     private void OnTriggerEnter2D (Collider2D other)
     {
+        // Если столкнулся с объектом Выход
         if (other.tag == "Exit")
         {
+            // Вызвать запуск следующего уровня с задержкой в 1 секунду
             Invoke("Restart", restartLevelDelay);
+            // отключаем объект игрока уровень пройден
             enabled = false;
         }
+        // Проверяем столкновение с едой
         else if (other.tag == "Food")
         {
+            // Добавляем очки еды игроку
             food += pointsPerFood;
+            // Удаляем съеденый объект
             other.gameObject.SetActive(false);
         }
+        // проверяем столкновение с Содовой
         else if (other.tag == "Soda")
         {
+            // добавляем очки еды
             food += pointsPerSoda;
+            // Удаляем съеденый объект 
             other.gameObject.SetActive(false);
         }
     }
 
+    // Если игрок сталкнулся со стеной, но начинает её атаковать
     protected override void OnCantMove<T>(T component)
     {
+        // Установить hitWall равным компоненту передающемуся в качестве параметра.
         Wall hitWall = component as Wall;
+        // Наносим стене удар
         hitWall.DamageWall(wallDamage);
+        // Проигрываем анимацию удара
         animator.SetTrigger("playerChop");
     }
 
+    // Вызываем перезапуск уровня
     private void Restart()
     {
+        // Загрузка последней сцены загружен, в этом случае Майне, только сцены в игре.
         Application.LoadLevel(Application.loadedLevel);
     }
 
+    // Вызывается при нападении врага с параметром количество полученных повреждений
     public void LooseFood(int loss)
     {
+        // Проигрываем анмацию получение повреждений
         animator.SetTrigger("playerHit");
+        // отнимаем очки еды
         food -= loss;
+        // проверяем живой ли игрок
         CheckIfGameOver();
     }
 
+    // проверка жив ли ещё игрок
     private void CheckIfGameOver()
     {
+        // если очков еди меньше или равно нулю
         if (food <= 0)
+            // Объевляем конец игры
             GameManager.instance.GameOver();
     }
 }
