b727e186be202fc666d82f1e4013246059c97863
Mellonkhv <mellonkhv@gmail.com>
2015-05-15 15:56:10 +1000

    2DPlatformer - эффект от выстрела

--- 2DPlatformer/Assets/2D Platformer/Scripts/Weapon.cs
+++ 2DPlatformer/Assets/2D Platformer/Scripts/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour {
     public LayerMask whatToHit;
 
     public Transform BulletTrailPrefab;
+    public Transform HitPrefab;
     public Transform MuzzleFlashPrefab;
     float timeToSpawnEffect = 0;
     public float effectSpawnRate = 10;
@@ -52,12 +53,8 @@ public class Weapon : MonoBehaviour {
         Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
         Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
         RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
-        if (Time.time >= timeToSpawnEffect)
-        {
-            Effect();
-            timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
-        }
-       // Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100, Color.cyan);
+        
+        Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100, Color.cyan);
         if (hit.collider != null)
         {
             Debug.DrawLine(firePointPosition, hit.point, Color.red);
@@ -68,12 +65,47 @@ public class Weapon : MonoBehaviour {
                 Debug.Log("Мы попали " + hit.collider.name + " и нанесли " + Damage + " урона.");
             }
         }
+        if (Time.time >= timeToSpawnEffect)
+        {
+            Vector3 hitPos;
+            Vector3 hitNormal;
+            if (hit.collider == null)
+            {
+                hitPos = (mousePosition - firePointPosition) * 30;
+                hitNormal = new Vector3(9999, 9999, 9999);
+            }
+            else
+            {
+                hitPos = hit.point;
+                hitNormal = hit.normal;
+            }
+
+            Effect(hitPos, hitNormal);
+            timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+        }
     }
 
-    void Effect()
+    void Effect(Vector3 hitPos, Vector3 hitNormal)
     {
-        Instantiate (BulletTrailPrefab, firePoint.position, firePoint.rotation);
-        Transform clone  = Instantiate (MuzzleFlashPrefab, firePoint.position, firePoint.rotation) as Transform;
+        Transform trail = Instantiate (BulletTrailPrefab, firePoint.position, firePoint.rotation) as Transform;
+        LineRenderer lr = trail.GetComponent<LineRenderer>();
+
+        if(lr != null)
+        {
+            // Устанавливаем позицию
+            lr.SetPosition(0, firePoint.position);
+            lr.SetPosition(1, hitPos);
+        }
+
+        Destroy(trail.gameObject, 0.04f);
+
+        if(hitNormal != new Vector3(9999, 9999, 9999))
+        {
+           Transform hitParticle = Instantiate(HitPrefab, hitPos, Quaternion.FromToRotation(Vector3.right, hitNormal)) as Transform;
+           Destroy(hitParticle.gameObject, 1f);
+        }
+
+        Transform clone = Instantiate (MuzzleFlashPrefab, firePoint.position, firePoint.rotation) as Transform;
         clone.parent = firePoint;
         float size = Random.Range (0.6f, 0.9f);
         clone.localScale = new Vector3(size, size, size);
