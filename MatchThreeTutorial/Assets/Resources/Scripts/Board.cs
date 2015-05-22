391ff69a5b8fcb040d878e67219008c153bacbf9
Mellonkhv <mellonkhv@gmail.com>
2015-05-22 16:44:04 +1000

    MatchThreeTutorial - Часть 3
    Добавили сферу в объект, добавили материал к сфере, выделение по клику в
    виде спецэффекта, добавили смену выделеных соседей местами

--- MatchThreeTutorial/Assets/Resources/Scripts/Board.cs
+++ MatchThreeTutorial/Assets/Resources/Scripts/Board.cs
@@ -9,8 +9,18 @@ public class Board : MonoBehaviour
     public int GridWidth;
     public int GridHeight;
     public GameObject GemPrefab;
+    public Gem LastGem;
+    public Vector3 Gem1Start;
+    public Vector3 Gem1End;
+    public Vector3 Gem2Start;
+    public Vector3 Gem2End;
+    public bool IsSwapping = false;
+    public Gem Gem1;
+    public Gem Gem2;
+    public float StartTime;
+    public float SwapRate = 2;
 
-	// Use this for initialization
+    // Use this for initialization
 	void Start () 
     {
 	    for (int y = 0; y < GridHeight; y++)
@@ -29,7 +39,70 @@ public class Board : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
     {
-	    
+	    if (IsSwapping)
+	    {
+	        MoveGem(Gem1, Gem1End,Gem1Start);
+            MoveNegGem(Gem2, Gem2End, Gem2Start);
+	        if (Vector3.Distance(Gem1.transform.position, Gem1End) < .1f ||
+	            Vector3.Distance(Gem2.transform.position, Gem2End) < .1f)
+	        {
+	            Gem1.transform.position = Gem1End;
+                Gem2.transform.position = Gem2End;
+	            IsSwapping = false;
+	        }
+	    }
 	}
+
+    public void MoveGem(Gem gemToMove, Vector3 toPos, Vector3 fromPos)
+    {
+        Vector3 center = (fromPos - toPos) *.5f;
+        center -= new Vector3(0,0,1f);
+        Vector3 riseRelCenter = fromPos - center;
+        Vector3 setRelCenter = toPos - center;
+        float fracComplete = (Time.time - StartTime)/SwapRate;
+        gemToMove.transform.position = Vector3.Slerp(riseRelCenter, setRelCenter, fracComplete);
+        gemToMove.transform.position += center;
+    }
+
+    public void MoveNegGem(Gem gemToMove, Vector3 toPos, Vector3 fromPos)
+    {
+        Vector3 center = (fromPos - toPos) * .5f;
+        center -= new Vector3(0, 0, -1f);
+        Vector3 riseRelCenter = fromPos - center;
+        Vector3 setRelCenter = toPos - center;
+        float fracComplete = (Time.time - StartTime) / SwapRate;
+        gemToMove.transform.position = Vector3.Slerp(riseRelCenter, setRelCenter, fracComplete);
+        gemToMove.transform.position += center;
+    }
+
+    public void SwapGems(Gem currentGem)
+    {
+        if (LastGem == null)
+            LastGem = currentGem;
+        else if (LastGem == currentGem)
+            LastGem = null;
+        else
+        {
+            if (LastGem.IsNeighborWith(currentGem))
+            {
+                Gem1Start = LastGem.transform.position;
+                Gem2Start = currentGem.transform.position;
+                Gem1End = currentGem.transform.position;
+                Gem2End = LastGem.transform.position;
+
+                StartTime = Time.time;
+
+                Gem1 = LastGem;
+                Gem2 = currentGem;
+                IsSwapping = true;
+            }
+            else
+            {
+                LastGem.ToggleSelector();
+                LastGem = currentGem;
+            }
+        }
+    }
+
 
 }
--- MatchThreeTutorial/Assets/Resources/Scripts/Gem.cs
+++ MatchThreeTutorial/Assets/Resources/Scripts/Gem.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
 
-public class Gem : MonoBehaviour 
+public class Gem : MonoBehaviour
 {
+    public GameObject Sphere;
+    public GameObject Selector;
+    private string[] _gemMats = {"Blue", "Green", "Grey", "Purple", "Red", "Yellow"};
+    private string _color = "";
     public List<Gem> Neighbors = new List<Gem>();
-
-	// Use this for initialization
+    public bool IsSelected = false;
+	
+    // Use this for initialization
 	void Start () 
     {
-	    
+	    CreateGem();
 	}
 	
 	// Update is called once per frame
@@ -18,9 +25,37 @@ public class Gem : MonoBehaviour
 	    
 	}
 
+    public void ToggleSelector()
+    {
+        IsSelected = !IsSelected;
+        Selector.SetActive(IsSelected);
+    }
+
+    
+
+    public void CreateGem()
+    {
+        //Destroy(Sphere);
+        _color = _gemMats[Random.Range(0, _gemMats.Length)];
+        Debug.Log(_color);
+        Material m = Resources.Load("Materials/" + _color) as Material;
+        Sphere.GetComponent<Renderer>().material = m;
+
+    }
+
     public void AddNeighbor(Gem g)
     {
-        Neighbors.Add(g);
+        if(!Neighbors.Contains(g))
+            Neighbors.Add(g);
+    }
+
+    public bool IsNeighborWith(Gem g)
+    {
+        if (Neighbors.Contains(g))
+        {
+            return true;
+        }
+        return false;
     }
 
     public void RemoveNeighbor(Gem g)
@@ -30,6 +65,7 @@ public class Gem : MonoBehaviour
 
     public void OnMouseDown()
     {
-        
+        ToggleSelector();
+        GameObject.Find("Board").GetComponent<Board>().SwapGems(this);
     }
 }
