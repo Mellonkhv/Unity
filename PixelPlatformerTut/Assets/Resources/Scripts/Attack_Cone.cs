45a257474b5f45916b991d06f8d8d3d57a329ebd
Mellonkhv <mellonkhv@gmail.com>
2015-06-25 16:12:14 +1000

    PixelPlatformer - Часть 12
    Турель ИИ. Стрельба и нанесение урона игроку.

---
+++ PixelPlatformerTut/Assets/Resources/Scripts/Attack_Cone.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack_Cone : MonoBehaviour
+{
+
+    public TurretAI TurretAi;
+
+    public bool IsLeft = false;
+
+    void Awake()
+    {
+        TurretAi = gameObject.GetComponentInParent<TurretAI>();
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+            TurretAi.Attack(!IsLeft);
+    }
+
+}
---
+++ PixelPlatformerTut/Assets/Resources/Scripts/TurretAI.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretAI : MonoBehaviour 
+{
+    public int CurHealth;
+    public int MaxHealth;
+
+    public float Distance;
+    public float WakeRange;
+    public float ShootInterval;
+    public float BulletSpeed = 100;
+    public float BulletTimer;
+
+    public bool AwakeTurret = false;
+    public bool LookingRight = true;
+
+    public GameObject Bullet;
+    public Transform Target;
+    public Animator Anim;
+    public Transform ShootPointLeft;
+    public Transform ShootPointRight;
+
+    void Awake()
+    {
+        Anim = gameObject.GetComponent<Animator>();
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+        CurHealth = MaxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Anim.SetBool("Awake", AwakeTurret);
+	    Anim.SetBool("LookingRight", LookingRight);
+
+        RangeCheck();
+
+	    LookingRight = Target.transform.position.x > transform.position.x;
+
+
+    }
+
+    void RangeCheck()
+    {
+        Distance = Vector3.Distance(transform.position, Target.transform.position);
+
+        AwakeTurret = Distance < WakeRange;
+    }
+
+    public void Attack(bool attackingRight)
+    {
+        BulletTimer += Time.deltaTime;
+
+        if (BulletTimer >= ShootInterval)
+        {
+            Vector2 direction = Target.transform.position - transform.position;
+            direction.Normalize();
+            if (!attackingRight)
+            {
+                var bulletClone = Instantiate(Bullet, ShootPointLeft.transform.position, ShootPointLeft.transform.rotation) as GameObject;
+                bulletClone.GetComponent<Rigidbody2D>().velocity = direction * BulletSpeed;
+
+                BulletTimer = 0;
+            }
+
+            if (attackingRight)
+            {
+                var bulletClone = Instantiate(Bullet, ShootPointRight.transform.position, ShootPointRight.transform.rotation) as GameObject;
+                bulletClone.GetComponent<Rigidbody2D>().velocity = direction * BulletSpeed;
+
+                BulletTimer = 0;
+            }
+        }
+    }
+}
