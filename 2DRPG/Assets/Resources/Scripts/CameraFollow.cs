8af876ec8b6639e8f43aa69d75d3489c208c5da1
Mellonkhv <mellonkhv@gmail.com>
2015-09-02 16:14:59 +1000

    Обновление

---
+++ 2DRPG/Assets/Resources/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    public float m_speed = 0.1f;
+    private Camera mycam;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    mycam = GetComponent<Camera>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    mycam.orthographicSize = (Screen.height / 100f) / 4f;
+
+	    if (Target)
+	    {
+	        transform.position = Vector3.Lerp(transform.position, Target.position, m_speed) + new Vector3(0, 0, -10);
+	    }
+	}
+}
